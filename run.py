b4a7b46990020d51f698f720ef293f0954da954c
JordanKingVeoxyl <veoxyl@gmail.com>
2023-05-20 14:09:30 +0000

    Added a function that displays the most popular counties in Ireland with their average ratings. The user is then prompt to select whether they would like to be educated on how to explore and visit one of the most popular counties, or if they would like to submit a rating for a county they've already visited and have an opinion of.

--- run.py
+++ run.py
@@ -16,3 +16,20 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_ireland')
+
+def county_and_rating(county, rating):
+    """
+    Returns a list of the most popular Irish counties & their associative ratings.
+    """
+    print("Here is a list of the three most popular counties in Ireland,")
+    print("along with their user ratings.\n")
+
+    for county, rating in zip(county, rating):
+        print(colored(
+            (f' County: {county}\n User rating: {rating} / 5 stars\n'),
+            'cyan'))
+
+    print("Enter '1' if you would like to learn how to explore a county you haven't been to yet!")
+    print("Enter '2' if you would like to submit a rating for a county you have already explored.")
+
+
